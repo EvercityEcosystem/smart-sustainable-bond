0dfdeaaa3d6a11673886d2d5d58d12aef33209da
BoogerWooger <boogerwooger@mail.ru>
2020-10-09 13:12:37 +0000

    pre-set accounts in genesis test variant

--- node/src/chain_spec.rs
+++ node/src/chain_spec.rs
@@ -1,8 +1,11 @@
-use sp_core::{Pair, Public, sr25519};
-use node_template_runtime::{
-	AccountId, AuraConfig, BalancesConfig, GenesisConfig, GrandpaConfig,
-	SudoConfig, SystemConfig, WASM_BINARY, Signature
+use sp_core::{Pair, Public, sr25519, crypto::{Ss58Codec, Ss58AddressFormat}};
+use evercity_runtime::{
+    AccountId, AuraConfig, BalancesConfig, GenesisConfig, GrandpaConfig,
+    SudoConfig, SystemConfig, WASM_BINARY, Signature, EvercityConfig
 };
+
+use evercity_runtime::evercity::{MASTER_ROLE_MASK, CUSTODIAN_ROLE_MASK, EMITENT_ROLE_MASK, INVESTOR_ROLE_MASK};
+
 use sp_consensus_aura::sr25519::AuthorityId as AuraId;
 use sp_finality_grandpa::AuthorityId as GrandpaId;
 use sp_runtime::traits::{Verify, IdentifyAccount};
@@ -16,142 +19,168 @@ pub type ChainSpec = sc_service::GenericChainSpec<GenesisConfig>;
 
 /// Generate a crypto pair from seed.
 pub fn get_from_seed<TPublic: Public>(seed: &str) -> <TPublic::Pair as Pair>::Public {
-	TPublic::Pair::from_string(&format!("//{}", seed), None)
-		.expect("static values are valid; qed")
-		.public()
+    TPublic::Pair::from_string(&format!("//{}", seed), None)
+        .expect("static values are valid; qed")
+        .public()
 }
 
 type AccountPublic = <Signature as Verify>::Signer;
 
 /// Generate an account ID from seed.
 pub fn get_account_id_from_seed<TPublic: Public>(seed: &str) -> AccountId where
-	AccountPublic: From<<TPublic::Pair as Pair>::Public>
+    AccountPublic: From<<TPublic::Pair as Pair>::Public>
 {
-	AccountPublic::from(get_from_seed::<TPublic>(seed)).into_account()
+    AccountPublic::from(get_from_seed::<TPublic>(seed)).into_account()
 }
 
 /// Generate an Aura authority key.
 pub fn authority_keys_from_seed(s: &str) -> (AuraId, GrandpaId) {
-	(
-		get_from_seed::<AuraId>(s),
-		get_from_seed::<GrandpaId>(s),
-	)
+    (
+        get_from_seed::<AuraId>(s),
+        get_from_seed::<GrandpaId>(s),
+    )
 }
 
-pub fn development_config() -> Result<ChainSpec, String> {
-	let wasm_binary = WASM_BINARY.ok_or("Development wasm binary not available".to_string())?;
 
-	Ok(ChainSpec::from_genesis(
-		// Name
-		"Development",
-		// ID
-		"dev",
-		ChainType::Development,
-		move || testnet_genesis(
-			wasm_binary,
-			// Initial PoA authorities
-			vec![
-				authority_keys_from_seed("Alice"),
-			],
-			// Sudo account
-			get_account_id_from_seed::<sr25519::Public>("Alice"),
-			// Pre-funded accounts
-			vec![
-				get_account_id_from_seed::<sr25519::Public>("Alice"),
-				get_account_id_from_seed::<sr25519::Public>("Bob"),
-				get_account_id_from_seed::<sr25519::Public>("Alice//stash"),
-				get_account_id_from_seed::<sr25519::Public>("Bob//stash"),
-			],
-			true,
-		),
-		// Bootnodes
-		vec![],
-		// Telemetry
-		None,
-		// Protocol ID
-		None,
-		// Properties
-		None,
-		// Extensions
-		None,
-	))
+pub fn development_config() -> Result<ChainSpec, String> {
+    let wasm_binary = WASM_BINARY.ok_or("Development wasm binary not available".to_string())?;
+
+    Ok(ChainSpec::from_genesis(
+        // Name
+        "Development",
+        // ID
+        "dev",
+        ChainType::Development,
+        move || testnet_genesis(
+            wasm_binary,
+            // Initial PoA authorities
+            vec![
+                authority_keys_from_seed("Alice"),
+            ],
+            // Sudo account
+            get_account_id_from_seed::<sr25519::Public>("Alice"),
+            // Pre-funded accounts
+            vec![
+                get_account_id_from_seed::<sr25519::Public>("Alice"),
+                get_account_id_from_seed::<sr25519::Public>("Bob"),
+                get_account_id_from_seed::<sr25519::Public>("Alice//stash"),
+                get_account_id_from_seed::<sr25519::Public>("Bob//stash"),
+            ],
+            true,
+        ),
+        // Bootnodes
+        vec![],
+        // Telemetry
+        None,
+        // Protocol ID
+        None,
+        // Properties
+        None,
+        // Extensions
+        None,
+    ))
 }
 
 pub fn local_testnet_config() -> Result<ChainSpec, String> {
-	let wasm_binary = WASM_BINARY.ok_or("Development wasm binary not available".to_string())?;
+    let wasm_binary = WASM_BINARY.ok_or("Development wasm binary not available".to_string())?;
 
-	Ok(ChainSpec::from_genesis(
-		// Name
-		"Local Testnet",
-		// ID
-		"local_testnet",
-		ChainType::Local,
-		move || testnet_genesis(
-			wasm_binary,
-			// Initial PoA authorities
-			vec![
-				authority_keys_from_seed("Alice"),
-				authority_keys_from_seed("Bob"),
-			],
-			// Sudo account
-			get_account_id_from_seed::<sr25519::Public>("Alice"),
-			// Pre-funded accounts
-			vec![
-				get_account_id_from_seed::<sr25519::Public>("Alice"),
-				get_account_id_from_seed::<sr25519::Public>("Bob"),
-				get_account_id_from_seed::<sr25519::Public>("Charlie"),
-				get_account_id_from_seed::<sr25519::Public>("Dave"),
-				get_account_id_from_seed::<sr25519::Public>("Eve"),
-				get_account_id_from_seed::<sr25519::Public>("Ferdie"),
-				get_account_id_from_seed::<sr25519::Public>("Alice//stash"),
-				get_account_id_from_seed::<sr25519::Public>("Bob//stash"),
-				get_account_id_from_seed::<sr25519::Public>("Charlie//stash"),
-				get_account_id_from_seed::<sr25519::Public>("Dave//stash"),
-				get_account_id_from_seed::<sr25519::Public>("Eve//stash"),
-				get_account_id_from_seed::<sr25519::Public>("Ferdie//stash"),
-			],
-			true,
-		),
-		// Bootnodes
-		vec![],
-		// Telemetry
-		None,
-		// Protocol ID
-		None,
-		// Properties
-		None,
-		// Extensions
-		None,
-	))
+    Ok(ChainSpec::from_genesis(
+        // Name
+        "Local Testnet",
+        // ID
+        "local_testnet",
+        ChainType::Local,
+        move || testnet_genesis(
+            wasm_binary,
+            // Initial PoA authorities
+            vec![
+                authority_keys_from_seed("Alice"),
+                authority_keys_from_seed("Bob"),
+            ],
+            // Sudo account
+            get_account_id_from_seed::<sr25519::Public>("Alice"),
+            // Pre-funded accounts
+            vec![
+                get_account_id_from_seed::<sr25519::Public>("Alice"),
+                get_account_id_from_seed::<sr25519::Public>("Bob"),
+                get_account_id_from_seed::<sr25519::Public>("Charlie"),
+                get_account_id_from_seed::<sr25519::Public>("Dave"),
+                get_account_id_from_seed::<sr25519::Public>("Eve"),
+                get_account_id_from_seed::<sr25519::Public>("Ferdie"),
+                get_account_id_from_seed::<sr25519::Public>("Alice//stash"),
+                get_account_id_from_seed::<sr25519::Public>("Bob//stash"),
+                get_account_id_from_seed::<sr25519::Public>("Charlie//stash"),
+                get_account_id_from_seed::<sr25519::Public>("Dave//stash"),
+                get_account_id_from_seed::<sr25519::Public>("Eve//stash"),
+                get_account_id_from_seed::<sr25519::Public>("Ferdie//stash"),
+            ],
+            true,
+        ),
+        // Bootnodes
+        vec![],
+        // Telemetry
+        None,
+        // Protocol ID
+        None,
+        // Properties
+        None,
+        // Extensions
+        None,
+    ))
 }
+
 
 /// Configure initial storage state for FRAME modules.
 fn testnet_genesis(
-	wasm_binary: &[u8],
-	initial_authorities: Vec<(AuraId, GrandpaId)>,
-	root_key: AccountId,
-	endowed_accounts: Vec<AccountId>,
-	_enable_println: bool,
+    wasm_binary: &[u8],
+    initial_authorities: Vec<(AuraId, GrandpaId)>,
+    root_key: AccountId,
+    endowed_accounts: Vec<AccountId>,
+    _enable_println: bool,
 ) -> GenesisConfig {
-	GenesisConfig {
-		frame_system: Some(SystemConfig {
-			// Add Wasm runtime to storage.
-			code: wasm_binary.to_vec(),
-			changes_trie_config: Default::default(),
-		}),
-		pallet_balances: Some(BalancesConfig {
-			// Configure endowed accounts with initial balance of 1 << 60.
-			balances: endowed_accounts.iter().cloned().map(|k|(k, 1 << 60)).collect(),
-		}),
-		pallet_aura: Some(AuraConfig {
-			authorities: initial_authorities.iter().map(|x| (x.0.clone())).collect(),
-		}),
-		pallet_grandpa: Some(GrandpaConfig {
-			authorities: initial_authorities.iter().map(|x| (x.1.clone(), 1)).collect(),
-		}),
-		pallet_sudo: Some(SudoConfig {
-			// Assign network admin rights.
-			key: root_key,
-		}),
-	}
+    
+    let _pre_master_account_id: AccountId = 
+        Ss58Codec::from_ss58check("5DJBx8EcrJqWqDQDe3xPd7Bw2zL3obvHigdLZKVGDHx7GRwW").unwrap();
+    let _pre_custodian_account_id: AccountId = 
+        Ss58Codec::from_ss58check("5EZ4JyMCxR6k5oDPAV5Bh1hqvMreqLZMbaXX2XUTk6f3ZPDL").unwrap();
+    let _pre_emitent_account_id: AccountId = 
+        Ss58Codec::from_ss58check("5FxdLBFRrE7NF3u2Tq95XE5gM1ve4YAd9ZnP8ZujUJ85gf7c").unwrap();
+    let _pre_investor_account_id: AccountId = 
+        Ss58Codec::from_ss58check("5FzuNtedbrnQrsKZKpAUzRy6swX9hM1PiLemREKoN2tBc3W1").unwrap();
+
+
+    GenesisConfig {
+        frame_system: Some(SystemConfig {
+            // Add Wasm runtime to storage.
+            code: wasm_binary.to_vec(),
+            changes_trie_config: Default::default(),
+        }),
+        pallet_balances: Some(BalancesConfig {
+            balances: endowed_accounts.iter().cloned().map(|k|(k, 1 << 60)).collect(),
+        }),
+        pallet_aura: Some(AuraConfig {
+            authorities: initial_authorities.iter().map(|x| (x.0.clone())).collect(),
+        }),
+        pallet_grandpa: Some(GrandpaConfig {
+            authorities: initial_authorities.iter().map(|x| (x.1.clone(), 1)).collect(),
+        }),
+        pallet_sudo: Some(SudoConfig {
+            key: root_key,
+        }),
+        evercity: Some(EvercityConfig {
+            // set roles for each pre-set accounts (set role)
+            genesis_accounts: [
+                (_pre_master_account_id.clone(), MASTER_ROLE_MASK),
+                (_pre_custodian_account_id.clone(), CUSTODIAN_ROLE_MASK),
+                (_pre_emitent_account_id.clone(), EMITENT_ROLE_MASK),
+                (_pre_investor_account_id.clone(), INVESTOR_ROLE_MASK),
+            ].iter().cloned().collect(),
+            
+            // set value (temporary - bool) for each specific map of accounts
+            genesis_master_accounts: [(_pre_master_account_id.clone(), true)].iter().cloned().collect(),
+            genesis_custodian_accounts: [(_pre_custodian_account_id.clone(), true)].iter().cloned().collect(),
+            genesis_emitent_accounts: [(_pre_emitent_account_id.clone(), true)].iter().cloned().collect(),
+            genesis_investor_accounts: [(_pre_investor_account_id.clone(), true)].iter().cloned().collect(),
+        }),
+    }
 }
--- pallets/evercity/src/lib.rs
+++ pallets/evercity/src/lib.rs
@@ -6,8 +6,9 @@ use frame_system::{self as system, ensure_signed};
 use frame_support::dispatch::DispatchResult;
 
 pub const MASTER_ROLE_MASK: u8 = 1u8;
-pub const EMITENT_ROLE_MASK: u8 = 2u8;
-pub const CUSTODIAN_ROLE_MASK: u8 = 4u8;
+pub const CUSTODIAN_ROLE_MASK: u8 = 2u8;
+pub const EMITENT_ROLE_MASK: u8 = 4u8;
+pub const INVESTOR_ROLE_MASK: u8 = 8u8;
 /*
 1u8,    // Master
 2u8,    // Emitent
@@ -17,75 +18,68 @@ pub const CUSTODIAN_ROLE_MASK: u8 = 4u8;
 32u8,   // Auditor
 */
 
-/*
-#[derive(Debug, PartialEq, Encode, Decode, Default)]
-pub struct EvercityAccountInfo {
-    pub roles_mask: u8,
-    pub nickname: u64,
-}
-*/
-
 pub trait Trait: frame_system::Trait {
-	type Event: From<Event<Self>> + Into<<Self as frame_system::Trait>::Event>;
+    type Event: From<Event<Self>> + Into<<Self as frame_system::Trait>::Event>;
 }
-
 
 decl_storage! {
-	trait Store for Module<T: Trait> as EvercityModule {
+    trait Store for Module<T: Trait> as Evercity {
         Balances get(fn accounts_map):
             map hasher(blake2_128_concat) T::AccountId => u128;
-        Account get(fn accounts):
-            map hasher(blake2_128_concat) T::AccountId => (u8, u8); //roles, flags
+        Account get(fn accounts) config(genesis_accounts):
+            map hasher(blake2_128_concat) T::AccountId => u8; //roles
 
         // maps to store role-specific info about accounts. "bool" will be replaced to structs
-        MasterAccount:
+        MasterAccount config(genesis_master_accounts):
             map hasher(blake2_128_concat) T::AccountId => bool;
-        EmitentAccount:
+        CustodianAccount config(genesis_custodian_accounts):
             map hasher(blake2_128_concat) T::AccountId => bool;
-        CustodianAccount:
+        EmitentAccount config(genesis_emitent_accounts):
             map hasher(blake2_128_concat) T::AccountId => bool;
-	}
+        InvestorAccount config(genesis_investor_accounts):
+            map hasher(blake2_128_concat) T::AccountId => bool;
+    }
 }
 
 
 decl_event!(
-	pub enum Event<T> where AccountId = <T as frame_system::Trait>::AccountId {
-		/// Event documentation should end with an array that provides descriptive names for event
-		/// parameters. [something, who]
+    pub enum Event<T> where AccountId = <T as frame_system::Trait>::AccountId {
+        /// Event documentation should end with an array that provides descriptive names for event
+        /// parameters. [something, who]
         
         // [TODO] document events 
-		AccountAddWithRoleAndFlags(AccountId, u8, u8),
-	}
+        AccountAddWithRole(AccountId, u8),
+    }
 );
 
 
 decl_error! {
-	pub enum Error for Module<T: Trait> {
-		NoneValue,
+    pub enum Error for Module<T: Trait> {
+        NoneValue,
         
         /// Account was already added and present in mapping
-		AccountToAddAlreadyExists,
-	}
+        AccountToAddAlreadyExists,
+    }
 }
 
 
 decl_module! {
-	pub struct Module<T: Trait> for enum Call where origin: T::Origin {
-		// Errors must be initialized if they are used by the pallet.
-		type Error = Error<T>;
+    pub struct Module<T: Trait> for enum Call where origin: T::Origin {
+        // Errors must be initialized if they are used by the pallet.
+        type Error = Error<T>;
 
-		// Events must be initialized if they are used by the pallet.
-		fn deposit_event() = default;
+        // Events must be initialized if they are used by the pallet.
+        fn deposit_event() = default;
 
         // writer functions here
-   		#[weight = 10_000]
-        fn add_account_with_role(origin, who: T::AccountId, role: u8, flags: u8) -> DispatchResult {
+        #[weight = 10_000]
+        fn add_account_with_role(origin, who: T::AccountId, role: u8) -> DispatchResult {
             // let _caller_account_id = ensure_signed(origin)?;
             ensure!(!Account::<T>::contains_key(&who), Error::<T>::AccountToAddAlreadyExists);
 
             // [TODO] add tests
 
-            Account::<T>::insert(&who, (role, flags));
+            Account::<T>::insert(&who, role);
             if (role & MASTER_ROLE_MASK) != 0 {
                 MasterAccount::<T>::insert(&who, true);        
             }
@@ -96,12 +90,12 @@ decl_module! {
                 CustodianAccount::<T>::insert(&who, true);        
             }
 
-            Self::deposit_event(RawEvent::AccountAddWithRoleAndFlags(who, role, flags));
+            Self::deposit_event(RawEvent::AccountAddWithRole(who, role));
             Ok(())
         }
 
-   /*
-		#[weight = 10_000]
+        /*
+        #[weight = 10_000]
         fn set_master_role_to_account(who: AccountId) -> DispatchResult {
             let _ = ensure_signed(origin)?;
             // let mut acc = Self::account(who).ok_or(Error::<T>::AccountNotExist)?;
