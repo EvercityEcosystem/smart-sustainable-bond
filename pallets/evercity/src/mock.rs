59c8dc1edc255d6933a2af6dfe671ec192ad8089
BoogerWooger <boogerwooger@mail.ru>
2020-10-15 11:43:15 +0000

    temp commit, add some balances to test

--- pallets/evercity/src/mock.rs
+++ pallets/evercity/src/mock.rs
@@ -1,4 +1,5 @@
 use crate::{Module, Trait};
+use pallet_balances;
 use sp_core::H256;
 use frame_support::{impl_outer_origin, parameter_types, weights::Weight};
 use sp_runtime::{
@@ -19,6 +20,7 @@ parameter_types! {
 	pub const MaximumBlockWeight: Weight = 1024;
 	pub const MaximumBlockLength: u32 = 2 * 1024;
 	pub const AvailableBlockRatio: Perbill = Perbill::from_percent(75);
+	pub const ExistentialDeposit: u64 = 0;
 }
 
 impl frame_system::Trait for TestRuntime {
@@ -43,7 +45,7 @@ impl frame_system::Trait for TestRuntime {
 	type AvailableBlockRatio = AvailableBlockRatio;
 	type Version = ();
 	type ModuleToIndex = ();
-	type AccountData = ();
+	type AccountData = pallet_balances::AccountData<u64>;
 	type OnNewAccount = ();
 	type OnKilledAccount = ();
 	type SystemWeightInfo = ();
@@ -54,10 +56,41 @@ impl Trait for TestRuntime {
 	type Event = ();
 }
 
+impl pallet_balances::Trait for TestRuntime {                                                                                        
+    type Balance = u64;                                                                                                       
+    type Event = ();                                                                                                   
+    type DustRemoval = ();                                                                                                    
+    type ExistentialDeposit = ExistentialDeposit;                                                                             
+    type AccountStore = System;                                                                          
+    type WeightInfo = ();                                                                                                     
+}
+
+pub type System = frame_system::Module<TestRuntime>;
 pub type Evercity = Module<TestRuntime>;
+pub type Balances = pallet_balances::Module<TestRuntime>;
 
 // Build genesis storage according to the mock runtime.
 pub fn new_test_ext() -> sp_io::TestExternalities {
-    let t = frame_system::GenesisConfig::default().build_storage::<TestRuntime>().unwrap();
-    t.into()
+    let mut t = frame_system::GenesisConfig::default().build_storage::<TestRuntime>().unwrap();
+	pallet_balances::GenesisConfig::<TestRuntime> {                                                                                  
+        // Provide some initial balances                                                                                      
+        balances: vec![ (1, 13000),
+						(2, 11000),
+						(3, 1000),
+						(4, 3000),
+						(5, 19000)
+					  ],                                                            
+    }                                                                                                                         
+    .assimilate_storage(&mut t)                                                                                               
+    .unwrap();
+	
+	/*
+	crate::GenesisConfig {
+		genesis_account_registry: vec![]
+	}                                                                                                   
+    .assimilate_storage(&mut t)                                                                            
+    .unwrap();
+	*/
+    
+	t.into()
 }
